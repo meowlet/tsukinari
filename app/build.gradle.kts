05b0dd1579d1dd52f6a05ade451d8d570bf1df40
meowlet <ngoclink13@gmail.com>
2024-03-23 19:12:06 +0700

    Change the version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.meow.tsukinari"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
